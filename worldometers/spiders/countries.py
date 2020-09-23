8e9d920132bfdaaaae37390c8ffff7b2563baa2b
Patrick Ly <patrick.ly17@gmail.com>
2020-09-23 00:51:33 -0700

    Updated countries crawl to get country name, year and population

--- worldometers/spiders/countries.py
+++ worldometers/spiders/countries.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import logging
 
 
 class CountriesSpider(scrapy.Spider):
@@ -16,18 +17,33 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
-            # Using manual way to set absolute url
+            ########## 1.) Using manual way to set absolute url##########
             # absolute_url = f'https://www.worldometers.info{link}'
 
-            # Helper method to get absolute url
+            ############ 2.) Helper method to get absolute url#############
             # absolute_url = response.urljoin(link)
-            # use with absolute url above
+            ########## use with absolute url above################
             # yield scrapy.Request(url=absolute_url)
 
-            yield response.follow(url=link)
+            # 3. Helper repsone method to link absolute with relative link
+            yield response.follow(url=link, callback=self.parse_country, meta={'country_name': name})
 
-            # pulling just country name and link
+            # first try  - pulling just country name and link
             # yield{
             #     'country_name': name,
             #     "country_link": link
             # }
+    def parse_country(self, response):
+        # logging.info(response.url)
+        name = response.request.meta['country_name']
+        rows = response.xpath(
+            "(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+            yield {
+                'country_name': name,
+                'year': year,
+                'population': population
+            }
