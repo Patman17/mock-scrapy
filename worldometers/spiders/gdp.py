72b255ed4552fe6eb81b24c4b182f7be1fc2bb99
Patrick Ly <patrick.ly17@gmail.com>
2020-09-24 22:28:23 -0700

    Scraping links

--- worldometers/spiders/gdp.py
+++ worldometers/spiders/gdp.py
@@ -4,22 +4,25 @@ import scrapy
 
 class GdpSpider(scrapy.Spider):
     name = 'gdp'
-    allowed_domains = [
-        'www.worldpopulationreview.com/']
+    allowed_domains = ['worldpopulationreview.com']
     start_urls = [
         'https://www.worldpopulationreview.com/countries/countries-by-national-debt/']
 
     def parse(self, response):
-        title = response.xpath("//td/h1/text()").get()
-        countries_1 = response.xpath("//td/a/text()").getall()
+        # title = response.xpath("//td/h1/text()").get()
+        # countries_1 = response.xpath("//td/a/text()").getall()
         countries = response.xpath("//td/a")
         for country in countries:
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
-        yield {
-            'title': title,
-            'countries_1': countries_1,
-            'country_name': name,
-            'country_link': link
-        }
+        # """1) Manual yielding just the items"""
+        # # yield {
+        # #     'title': title,
+        # #     'countries_1': countries_1,
+        # #     'country_name': name,
+        # #     'country_link': link
+        # # }
+            """2) Utilize response to access the links"""
+            absolute_url = f'https://worldpopulationreview.com{link}'
+            yield scrapy.Request(url=absolute_url)
