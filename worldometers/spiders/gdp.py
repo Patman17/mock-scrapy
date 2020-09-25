c15a90796c57a24859b1266ff2034b9223fd2e60
Patrick Ly <patrick.ly17@gmail.com>
2020-09-24 22:57:04 -0700

    Redid the scraping to make it dictionary

--- worldometers/spiders/gdp.py
+++ worldometers/spiders/gdp.py
@@ -10,26 +10,35 @@ class GdpSpider(scrapy.Spider):
         'https://www.worldpopulationreview.com/countries/countries-by-national-debt/']
 
     def parse(self, response):
-        # title = response.xpath("//td/h1/text()").get()
-        # countries_1 = response.xpath("//td/a/text()").getall()
-        countries = response.xpath("//td/a")
-        name_list = []
-        link_list = []
-        for country in countries:
-            name = country.xpath(".//text()").get()
-            link = country.xpath(".//@href").get()
-            name_list.append(name)
-            link_list.append(link)
+        rows = response.xpath("//table/tbody/tr")
+        for row in rows:
+            yield{
+                'country_name': row.xpath('.//td[1]/a/text()').get(),
+                'gdp_debt': row.xpath('.//td[2]/text()').get(),
+                'population': row.xpath('.//td[3]/text()').get(),
+            }
 
-        GDP = response.xpath("//tr/td[2]/text()").getall()
-        population = response.xpath("//tr/td[3]/text()").getall()
+    # def parse(self, response):
+    #     # title = response.xpath("//td/h1/text()").get()
+    #     # countries_1 = response.xpath("//td/a/text()").getall()
+    #     countries = response.xpath("//td/a")
+    #     name_list = []
+    #     link_list = []
+    #     for country in countries:
+    #         name = country.xpath(".//text()").get()
+    #         link = country.xpath(".//@href").get()
+    #         name_list.append(name)
+    #         link_list.append(link)
 
-        yield {
-            'country_name': name_list,
-            'country_link': link_list,
-            'GDP': GDP,
-            'Population': population
-        }
+    #     GDP = response.xpath("//tr/td[2]/text()").getall()
+    #     population = response.xpath("//tr/td[3]/text()").getall()
+
+    #     yield {
+    #         'country_name': name_list,
+    #         'country_link': link_list,
+    #         'GDP': GDP,
+    #         'Population': population
+    #     }
 
         # """1) Manual yielding just the items"""
         # # yield {
